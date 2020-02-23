926c9f781826e40dec7b9716c63bb6c28ca70b3b
mbagatini <morganabagatini@gmail.com>
2020-02-23 11:23:07 -0300

    socket.io

--- src/server.js
+++ src/server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.server(app);
+const io = socketio(server);
+
+io.on('connection', socket => {
+    console.log('Usuario conectado', socket.id);
+});
 
 // Conecta no mongoDB atlas - porta 2707
 mongoose.connect('mongodb+srv://cluster:cluster@mycluster-zpv4l.mongodb.net/airbnb?retryWrites=true&w=majority', {
@@ -18,4 +28,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3030);
+server.listen(3030);

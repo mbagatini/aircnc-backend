7f563c8e9bb86cde509b4768d82278c5e6c00c57
mbagatini <morganabagatini@gmail.com>
2020-02-23 02:27:35 -0300

    Imagem JSON

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
@@ -13,9 +14,8 @@ mongoose.connect('mongodb+srv://cluster:cluster@mycluster-zpv4l.mongodb.net/airb
 
 // Controla quem pode acessar a API
 app.use(cors());
-
 app.use(express.json());
-
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3030);
